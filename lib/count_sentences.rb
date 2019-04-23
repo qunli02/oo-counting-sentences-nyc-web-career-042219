9829d7248beaac3879cf02ded0bd3eaa32b6aba4
Your Namessh-keygen <qunli02@gmail.com>
2019-04-23 19:03:43 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+      array = []
+      self.split(".").each do |two|
+        array << two.split("?")
+      end
+      array.flatten!
+      answer = []
+      array.each do |two|
+        answer << two.split("!")
+      end
+    answer.flatten!
+    answer.delete("")
+    answer.length
   end
 end
